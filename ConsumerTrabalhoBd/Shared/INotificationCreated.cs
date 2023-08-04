a56b5e52b8e32293ecf2ab0fe106201b2cc84099
Giulianno Ramos <giuleramos@gmail.com>
2023-08-04 15:04:03 -0300

    Update shared

--- ConsumerTrabalhoBd/Shared/INotificationCreated.cs
+++ ConsumerTrabalhoBd/Shared/INotificationCreated.cs
@@ -2,7 +2,6 @@ namespace TrabalhoBancoDeDados.Shared;
 
 public interface INotificationCreated
 {
-    public string Nome { get; set; }
-    public string Endereco { get; set; }
-    public int Telefone { get; set; }
+    public string Nome { get; set; }   
+    public string Telefone { get; set; }
 }
